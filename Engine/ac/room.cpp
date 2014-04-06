a78cc558b1b46b3bf21b6f9076e034cfe6284ba2
Benjamin Penney <penneyb@tpg.com.au>
2014-04-06 17:47:05 +1000

    Merged resolution switching stuff.

--- Engine/ac/room.cpp
+++ Engine/ac/room.cpp
@@ -422,14 +422,14 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
     // load the room from disk
     our_eip=200;
     thisroom.gameId = NO_GAME_ID_IN_ROOM_FILE;
-    load_room(room_filename, &thisroom, (game.default_resolution > 2));
+    load_room(room_filename, &thisroom, game.IsHiRes());
 
     if ((thisroom.gameId != NO_GAME_ID_IN_ROOM_FILE) &&
         (thisroom.gameId != game.uniqueid)) {
             quitprintf("!Unable to load '%s'. This room file is assigned to a different game.", room_filename.GetCStr());
     }
 
-    if ((game.default_resolution > 2) && (game.options[OPT_NATIVECOORDINATES] == 0))
+    if (game.IsHiRes() && (game.options[OPT_NATIVECOORDINATES] == 0))
     {
         convert_room_coordinates_to_low_res(&thisroom);
     }
@@ -503,7 +503,7 @@ void load_new_room(int newnum, CharacterInfo*forchar) {
         int newScreenHeight = final_scrn_hit;
         if (multiply_up_coordinate(thisroom.height) < final_scrn_hit) {
             clear_letterbox_borders();
-            newScreenHeight = get_fixed_pixel_size(200);
+            newScreenHeight = thisroom.height;
         }
 
         if (newScreenHeight == _sub_screen->GetHeight())
